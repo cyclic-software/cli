921146e0bae8995327f487bfe4759a2efabb9e0b
Kam Lasater <ckl@seekayel.com>
2021-01-13 10:26:31 -0500

    Added profile arg and eslinting

--- src/commands/init-app.ts
+++ src/commands/init-app.ts
@@ -8,7 +8,7 @@ export default class InitApp extends Command {
   static description = 'Will initialize the an application in the AWS account you are credentialed against.'
 
   static examples = [
-    `$ cyclic init-app --name=$NAME
+    `$ cyclic init-app --name $NAME --profile $PROFILE
 * Checking AWS account for bootstrapping
 * Checking AWS account for required roles
 * Creating Cyclic App: $NAME
@@ -19,7 +19,8 @@ export default class InitApp extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'application name'}),
+    name: flags.string({char: 'n', description: 'application name (defaults to random string)'}),
+    profile: flags.string({char: 'p', description: 'aws credential profile (defaults to default)'}),
   }
 
   // static args = [{name: '-n awesome-app-name'}]
@@ -31,33 +32,34 @@ export default class InitApp extends Command {
     const {flags} = this.parse(InitApp)
 
     const name = flags.name ?? 'second-default-string'
+    const profile = flags.profile ?? 'default'
     // const roleArn = ''
     // const roleSecret = ''
 
-    // Set the AWS Region and client params
-    const REGION = 'us-east-2'
+    // Set the AWS profile and client params
     const cfn = new CloudFormationClient({
-      region: REGION,
-      credentials: fromIni({profile: 'default'}),
+      // region: REGION, // 'us-east-2'
+      credentials: fromIni({profile: profile}),
     })
 
-    logger('* Checking AWS account for bootstrapping')
-    const listStacks = async () => {
-      try {
-        const data = await cfn.send(new DescribeStacksCommand({
-          StackName: 'CyclicBootstrapStack',
-        }))
-        const stacks: Stack[] = data?.Stacks ?? []
+    // let stack:Stack|string
 
-        stacks.forEach((s: Stack) => {
-          logger(JSON.stringify(s.Outputs))
-        })
-        // logger(stacks[0]?.Outputs ?? '':String);
-      } catch (error) {
-        logger('Error you probably need a bootstrap stack', error)
-      }
+    logger('* Checking AWS account for bootstrapping')
+    try {
+      const data = await cfn.send(new DescribeStacksCommand({
+        StackName: 'CyclicBootstrapStack',
+      }))
+      const stacks: Stack[] = data?.Stacks ?? []
+
+      stacks.forEach((s: Stack) => {
+        logger(JSON.stringify(s.Outputs))
+      })
+      // stack = stacks.pop() ?? ''
+      // logger(stacks[0]?.Outputs ?? '':String);
+    } catch (error) {
+      logger('Error you probably need a bootstrap stack', error)
+      return
     }
-    listStacks()
 
     logger('* Checking AWS account for required roles')
     logger(`* Creating Cyclic App: ${name}`)
@@ -65,6 +67,7 @@ export default class InitApp extends Command {
 
     // axios.post('https://api.cyclic.sh/v1/app/', {
     //   name: name,
+    //   stack: stack,
     //   roleArn: roleArn,
     //   roleSecret: roleSecret // pragma: allowlist secret
     //   })
